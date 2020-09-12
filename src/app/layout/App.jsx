5c5f99fa141056eb2563f9b036f0f8af44265c21
Aluru Murali Krishna <alurumuralikrishna@Alurus-MacBook-Pro.local>
2020-09-13 00:43:08 +0530

    Added NavBar, EventDashboard, EventList, EventListItem and EventForm components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Fragment, useState } from 'react';
+import EventDashboard from '../../features/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className='App'>
-      <h1>Re-vents</h1>
-    </div>
+    <Fragment>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ src/features/eventDashboard/EventDashboard.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Grid } from 'semantic-ui-react';
+
+import { sampleData } from '../../app/api/sampleData';
+
+import EventList from './EventList';
+import EventForm from './eventForm/EventForm';
+
+const EventDashboard = ({ formOpen, setFormOpen }) => {
+  const [events, setEevents] = useState(sampleData);
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default EventDashboard;
