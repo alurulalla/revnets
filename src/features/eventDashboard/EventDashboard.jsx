c17291e0203afe83b2257f19a5555ad6d58a5569
Aluru Murali Krishna <alurumuralikrishna@Alurus-MacBook-Pro.local>
2020-09-13 02:03:53 +0530

    Added CRUD operations

--- src/features/eventDashboard/EventDashboard.jsx
+++ src/features/eventDashboard/EventDashboard.jsx
@@ -6,16 +6,48 @@ import { sampleData } from '../../app/api/sampleData';
 import EventList from './EventList';
 import EventForm from './eventForm/EventForm';
 
-const EventDashboard = ({ formOpen, setFormOpen }) => {
+const EventDashboard = ({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}) => {
   const [events, setEevents] = useState(sampleData);
+
+  const handleCreateEvent = (event) => {
+    setEevents([...events, event]);
+  };
+
+  const handleUpdateEvent = (event) => {
+    setEevents(events.map((evt) => (evt.id === event.id ? event : evt)));
+    selectEvent(null);
+  };
+
+  const handleDeleteEvent = (eventID) => {
+    setEevents(events.filter((evt) => evt.id !== eventID));
+    setFormOpen(false);
+  };
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} />
+        <EventList
+          events={events}
+          selectEvent={selectEvent}
+          deleteEvent={handleDeleteEvent}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEevents={setEevents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+            updateEvent={handleUpdateEvent}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
